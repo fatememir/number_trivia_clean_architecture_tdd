aeee369ba3aff42033ced3e731836d0af561cd5d
fateme <fatememir>
2022-11-23 01:45:03 +0330

    create entity

--- lib/feature/number_trivia/domain/entity/number_trivia.dart
+++ lib/feature/number_trivia/domain/entity/number_trivia.dart
@@ -3,7 +3,7 @@ import 'package:equatable/equatable.dart';
 class NumberTrivia extends Equatable {
   final String text;
   final int number;
-
+  
   const NumberTrivia({
     required this.text,
     required this.number,
