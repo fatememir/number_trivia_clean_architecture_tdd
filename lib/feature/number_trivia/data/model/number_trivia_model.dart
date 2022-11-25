9d9ad97fe43725076f38c9f483ed5ea1e2f88369
fateme <fatememir>
2022-11-25 23:39:25 +0330

    implement number trivia model

---
+++ lib/feature/number_trivia/data/model/number_trivia_model.dart
@@ -0,0 +1,22 @@
+import 'package:number_trivia_clean_architecture_tdd/feature/number_trivia/domain/entity/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    required String text,
+    required int number,
+  }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+}
